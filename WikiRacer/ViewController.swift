e861ea246b53fd3b355e31d72f6f3d5ccb463319
manuel <maniponce22@gmail.com>
2021-02-28 23:35:12 -0600

    Adding WikipediaKit Library

--- WikiRacer/ViewController.swift
+++ WikiRacer/ViewController.swift
@@ -6,12 +6,28 @@
 //
 
 import UIKit
+import WikipediaKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        WikipediaNetworking.appAuthorEmailForAPI = "maniponce22@gmail.com"
+        
+        let language = WikipediaLanguage("en")
+
+        let _ = Wikipedia.shared.requestOptimizedSearchResults(language: language, term: "Serial Experiments Lain") { (searchResults, error) in
+
+            guard error == nil else { return }
+            guard let searchResults = searchResults else { return }
+
+            for articlePreview in searchResults.items {
+                print(articlePreview.displayTitle)
+            }
+        }
+        
     }
 
 
