4945ad19db0594c25ac6e7e87a7baea75c8964ac
Tim Nguyen <nguye@MacBook-Pro-2.local>
2021-03-01 00:05:29 -0600

    Testing Wiki articles

--- WikiRacer/ViewController.swift
+++ WikiRacer/ViewController.swift
@@ -18,13 +18,23 @@ class ViewController: UIViewController {
         
         let language = WikipediaLanguage("en")
 
-        let _ = Wikipedia.shared.requestOptimizedSearchResults(language: language, term: "Serial Experiments Lain") { (searchResults, error) in
+//        let _ = Wikipedia.shared.requestOptimizedSearchResults(language: language, term: "Serial Experiments Lain") { (searchResults, error) in
+//
+//            guard error == nil else { return }
+//            guard let searchResults = searchResults else { return }
+//
+//            for articlePreview in searchResults.items {
+//                print(articlePreview.displayTitle)
+//            }
+//        }
 
-            guard error == nil else { return }
-            guard let searchResults = searchResults else { return }
-
-            for articlePreview in searchResults.items {
-                print(articlePreview.displayTitle)
+        let _ = Wikipedia.shared.requestArticle(language: language, title: "Soft rime", imageWidth: 640) { result in
+            switch result {
+            case .success(let article):
+              print(article.displayTitle)
+              print(article.displayText)
+            case .failure(let error):
+              print(error)
             }
         }
         
