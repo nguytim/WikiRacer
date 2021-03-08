5286b507b54d005959ac70042f8c2bf98c257fd9
manuel <maniponce22@gmail.com>
2021-03-07 18:01:12 -0600

    New initial screen

---
+++ WikiRacer/View Controllers/LoginPageViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LoginPageViewController.swift
+//  WikiRacer
+//
+//  Created by Manuel on 3/7/21.
+//
+
+import UIKit
+import WikipediaKit
+
+class LoginPageViewController: UIViewController, UITextViewDelegate {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
