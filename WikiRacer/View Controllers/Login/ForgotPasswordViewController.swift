b6a862874800e2c6a4ba59d539ee356f5aafc62c
manuel <maniponce22@gmail.com>
2021-03-14 19:03:38 -0500

    Send reset link connected to firebase

--- WikiRacer/View Controllers/Login/ForgotPasswordViewController.swift
+++ WikiRacer/View Controllers/Login/ForgotPasswordViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ForgotPasswordViewController: UIViewController {
     
@@ -29,6 +30,7 @@ class ForgotPasswordViewController: UIViewController {
         self.view.endEditing(true)
     }
     
+    //Function 2 make our text field to look cool.
     private func setupTextField() {
         let usernameBorder = CALayer()
         let borderWidth = CGFloat(2.0)
@@ -42,11 +44,50 @@ class ForgotPasswordViewController: UIViewController {
                                                                          attributes: [NSAttributedString.Key.foregroundColor: UIColor.white])
     }
     
+    //Function to make our buttons look cool and styalized.
     private func setupButton() {
         let attributedString = NSAttributedString(string: NSLocalizedString("Send Reset Link", comment: ""), attributes: [NSAttributedString.Key.font : UIFont.systemFont(ofSize: 20.0), NSAttributedString.Key.foregroundColor : UIColor.white, NSAttributedString.Key.underlineStyle:1.0] as [NSAttributedString.Key : Any])
         sendResetLinkButton.setAttributedTitle(attributedString, for: .normal)
         sendResetLinkButton.setTitleColor(.white, for: .normal)
     }
+    
+    //Code that runs when 'Send Reset Link' is clicked.
+    //This code will check the email entered by the user and
+    //      Firebase will authenticate it for us and send
+    //      the user an email to that account to reset their password.
+    @IBAction func sendResetLinkButton(_ sender: Any) {
+        if !emailAddressTextField.text!.isEmpty {
+            let emailAddress = emailAddressTextField.text!
+
+            Auth.auth().sendPasswordReset(withEmail: emailAddress) {error in
+                if error != nil {
+                    //Bad Email, let the user know.
+                    let controller = UIAlertController(title: "Bad Email",
+                                                       message: "Please enter a valid email to an account.",
+                                                       preferredStyle: .alert)
+                    
+                    controller.addAction(UIAlertAction(title: "OK",
+                                                       style: .default,
+                                                       handler: nil))
+                    self.present(controller, animated: true, completion: nil)
+                }
+                else {
+                    //Succesful email entry and set the user a reset to that email.
+                    let controller = UIAlertController(title: "Email sent",
+                                                       message: "Please wait a few moments for the email to reach you. Happy Racing!",
+                                                       preferredStyle: .alert)
+                    
+                    controller.addAction(UIAlertAction(title: "OK",
+                                                       style: .default,
+                                                       handler: nil))
+                    self.present(controller, animated: true, completion: nil)
+                }
+            }
+        }
+        
+    }
+    
+    
     
     /*
     // MARK: - Navigation
