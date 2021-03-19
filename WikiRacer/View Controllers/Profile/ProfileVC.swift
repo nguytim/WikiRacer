8e0a660748cb53c11db0e5939326bb29d7bec5ef
Manuel Ponce <manuelp@wash.cs.utexas.edu>
2021-03-19 12:42:14 -0500

    loaded stats and profile

--- WikiRacer/View Controllers/Profile/ProfileVC.swift
+++ WikiRacer/View Controllers/Profile/ProfileVC.swift
@@ -6,16 +6,63 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
 
 class ProfileVC: UIViewController {
+    var db: Firestore!
 
+    @IBOutlet weak var leastNumLinksLabel: UILabel!
+    @IBOutlet weak var fastestTimeLabel: UILabel!
+    @IBOutlet weak var avgLinksLabel: UILabel!
+    @IBOutlet weak var avgGameTimeLabel: UILabel!
+    @IBOutlet weak var numGamesWonLabel: UILabel!
+    @IBOutlet weak var numGamesLabel: UILabel!
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var racerImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        let settings = FirestoreSettings()
+        
+        Firestore.firestore().settings = settings
+        //[End setup]
+        db = Firestore.firestore()
+        
         // Do any additional setup after loading the view.
     }
+    override func viewWillAppear(_ animated: Bool) {
+        loadStats()
+    }
     
-
+    
+    func loadStats() {
+        let docRef = db!.collection("users").document(Auth.auth().currentUser!.uid)
+        docRef.getDocument { (document, error) in
+            if let document = document, document.exists {
+                let data = document.data()
+                let userName = data!["username"] as! String
+                let numGames = data!["gamesPlayed"] as! Int
+                let gamesWon = data!["gamesWon"] as! Int
+                let avgTime = data!["averageGameTime"] as! Int
+                let avgLinks = data!["averageNumberOfLinks"] as! Int
+                let fastestTime = data!["fastestGame"] as!  Int
+                let leastNumLinks = data!["leastNumberofLink"] as! Int
+                
+                self.numGamesLabel.text = String(numGames)
+                self.numGamesWonLabel.text = String(gamesWon)
+                self.fastestTimeLabel.text = String(fastestTime)
+                self.leastNumLinksLabel.text = String(leastNumLinks)
+                self.userNameLabel.text = String(userName)
+                self.avgLinksLabel.text = String(avgLinks)
+                self.avgGameTimeLabel.text = String(avgTime)
+                
+                
+            }
+        }
+        
+    }
     /*
     // MARK: - Navigation
 
@@ -27,3 +74,4 @@ class ProfileVC: UIViewController {
     */
 
 }
+
